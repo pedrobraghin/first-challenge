c33b5f43a605fea8f1e064a37b1712a5d99b1a92
Pedro Braghin <braghindev@gmail.com>
2023-02-08 19:03:55 -0300

    WIP: feat: add controllers to event routes router

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,11 +1,37 @@
 import { Router } from 'express';
+import {
+  createEventController,
+  getAllEventsController,
+  getEventsByWeekdayController,
+  getEventByIdController,
+  deleteEventByIdController,
+  deleteEventsByWeekdayController,
+} from '../use-cases/event/index';
 
 const eventRouter = Router();
 
-eventRouter.get('/');
-eventRouter.get('/:weekday');
-eventRouter.get('/:id');
-eventRouter.delete(':weekday');
-eventRouter.delete('/:id');
+eventRouter.post('/', (req, res) => {
+  createEventController.handle(req, res);
+});
+
+eventRouter.get('/', (req, res) => {
+  getAllEventsController.handle(req, res);
+});
+
+eventRouter.get('/:weekday', (req, res) => {
+  getEventsByWeekdayController.handle(req, res);
+});
+
+eventRouter.get('/:id', (req, res) => {
+  getEventByIdController.handle(req, res);
+});
+
+eventRouter.delete(':weekday', (req, res) => {
+  deleteEventsByWeekdayController.handle(req, res);
+});
+
+eventRouter.delete('/:id', (req, res) => {
+  deleteEventByIdController.handle(req, res);
+});
 
 export { eventRouter };
