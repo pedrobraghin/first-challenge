464bc80e1ac3c4fea57ab18a051b788316c15f0f
Pedro Braghin <braghindev@gmail.com>
2023-02-08 20:42:41 -0300

    WIP: feat: add implementation to InMemoryEventsRepository and IEventRepository

--- src/repositories/IEventRepository.ts
+++ src/repositories/IEventRepository.ts
@@ -1,3 +1,10 @@
+import { Event } from '../entities/Event';
+
 export interface IEventRepository {
+  createEvent(event: Event): void;
   getAllEvents(): Event[];
+  getEventById(id: string): Event | null;
+  getEventsByWeekday(weekday: number): Event[] | null;
+  deleteEventById(id: string): Event | null;
+  deleteEventsByWeekday(weekday: number): Event[] | null;
 }
--- src/repositories/implementations/InMemoryEventsRepository.ts
+++ src/repositories/implementations/InMemoryEventsRepository.ts
@@ -1,7 +1,50 @@
+import { Event } from './../../entities/Event';
 import { IEventRepository } from './../IEventRepository';
 
 export class InMemoryEventsRepository implements IEventRepository {
+  private events: Event[] = [];
+
+  createEvent(event: Event): void {
+    this.events.push(event);
+  }
+
   getAllEvents(): Event[] {
-    throw new Error('Method not implemented.');
+    return this.events;
+  }
+
+  getEventById(id: string): Event | null {
+    return this.events.find((event) => event._id === id) ?? null;
+  }
+
+  getEventsByWeekday(weekday: number): Event[] | null {
+    const foundEvents = this.events.filter((event) => {
+      const eventDate = new Date(event.dateTime).getDay();
+      return eventDate === weekday;
+    });
+
+    return foundEvents;
+  }
+
+  deleteEventById(id: string): Event | null {
+    const eventIndex = this.events.findIndex((event) => event._id === id);
+    if (!eventIndex) {
+      return null;
+    }
+
+    return this.events.splice(eventIndex, 1)[0];
+  }
+
+  deleteEventsByWeekday(weekday: number): Event[] | null {
+    const deletedEvents: Event[] = [];
+    const filteredEvents = this.events.filter((eventToRemove) => {
+      const eventDate = new Date(eventToRemove.dateTime).getDay();
+      if (eventDate === weekday) {
+        deletedEvents.push(eventToRemove);
+      }
+      return eventDate != weekday;
+    });
+    this.events = filteredEvents;
+
+    return deletedEvents;
   }
 }
