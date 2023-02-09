b146b5ce5e778be89c86a52393d067557e32d969
Pedro Braghin <braghindev@gmail.com>
2023-02-09 15:04:26 -0300

    chore: setup test environment

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  clearMocks: true,
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  testMatch: ['**/**/*.spec.ts'],
+};
