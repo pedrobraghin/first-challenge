90b8636c43ee7d8353f21ff83bc54cca96ba3595
Pedro Braghin <braghindev@gmail.com>
2023-02-09 11:09:41 -0300

    refactor: improve the return of 'getEventsByWeekday' method to match the interface 'IEventRepository'

--- src/repositories/implementations/InMemoryEventsRepository.ts
+++ src/repositories/implementations/InMemoryEventsRepository.ts
@@ -22,6 +22,10 @@ export class InMemoryEventsRepository implements IEventRepository {
       return eventDate === weekday;
     });
 
+    if (foundEvents.length === 0) {
+      return null;
+    }
+
     return foundEvents;
   }
 
