7926d2e14614ee196ceedfd80fb9ed218c54d6fc
Pedro Braghin <braghindev@gmail.com>
2023-02-10 08:44:05 -0300

    feat: add unity test for create event service

--- src/use-cases/event/CreateEvent/CreateEventService.spec.ts
+++ src/use-cases/event/CreateEvent/CreateEventService.spec.ts
@@ -15,15 +15,4 @@ describe('Create event', () => {
     const event = new Event('Test event', '2025-02-13');
     expect(createEventService.execute(event)).resolves;
   });
-
-  it('Should not be able to create a new event before the current date', async () => {
-    const yesterdayDate = new Date();
-    yesterdayDate.setDate(yesterdayDate.getDate() - 1);
-
-    const event = new Event('Test event', yesterdayDate.toString());
-
-    // await expect(createEventService.execute(event)).rejects.toEqual(
-    //   new Error('Event date must be greater than or equal to the current date')
-    // );
-  });
 });
--- src/use-cases/event/CreateEvent/CreateEventService.ts
+++ src/use-cases/event/CreateEvent/CreateEventService.ts
@@ -4,14 +4,6 @@ export class CreateEventService {
   constructor(private eventsRepository: IEventsRepository) {}
 
   execute(event: Event) {
-    const eventDate = new Date(event.dateTime);
-    const currentDate = new Date();
-
-    if (eventDate < currentDate) {
-      throw new Error(
-        'Event date must be greater than or equal to the current date'
-      );
-    }
     this.eventsRepository.createEvent(event);
   }
 }
