be2ac7154d0c4fcaa97dbd570263f9ae7db67f14
Pedro Braghin <braghindev@gmail.com>
2023-02-12 18:53:47 -0300

    fix: change the getDayOfTheWeek method to consider 0 as sunday

--- src/utils/getDayOfTheWeek.ts
+++ src/utils/getDayOfTheWeek.ts
@@ -2,13 +2,13 @@ import { DaysOfTheWeek } from '../entities/DaysOfTheWeek';
 
 export const getDayOfTheWeek = (dayOfTheWeek: string) => {
   const days: DaysOfTheWeek = {
-    monday: 0,
-    tuesday: 1,
-    wednesday: 2,
-    thursday: 3,
-    friday: 4,
-    saturday: 5,
-    sunday: 6,
+    sunday: 0,
+    monday: 1,
+    tuesday: 2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6,
   };
   const day = days[dayOfTheWeek];
   if (day == undefined) {
