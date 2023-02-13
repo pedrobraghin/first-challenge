213767c93e33bd830c7d167f5b24f28a4f999414
Pedro Braghin <braghindev@gmail.com>
2023-02-13 08:56:26 -0300

    refactor: add port to '.env' file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { app } from './app';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log('Server is running on port: ' + port);
